5df00a23a7b11303b9d1fb47eebf76b4ea7ae5ad
murer <murer@dextra-sw.com>
2021-04-13 11:47:30 -0300

    init

---
+++ pgp/cryptpoc_test.go
@@ -0,0 +1,57 @@
+package pgp
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"testing"
+
+	"github.com/murer/vaultz/util"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPocSequential(t *testing.T) {
+	s := SymKeyGenerate()
+	kp := KeyGenerate("me", "me@sample.com")
+
+	buf := new(bytes.Buffer)
+	func() {
+		encrypter := EncypterCreate(buf, kp, KeyRingCreate(kp))
+		defer encrypter.Close()
+		writer := encrypter.Encrypt()
+		writer.Write([]byte("first"))
+	}()
+	buf.Write([]byte("\n"))
+
+	func() {
+		encrypter := SymEncypterCreate(buf, s)
+		defer encrypter.Close()
+		writer := encrypter.Encrypt()
+		writer.Write([]byte("second"))
+	}()
+
+	log.Printf("%s\n", buf.String())
+
+	func() {
+		decrypter := DecrypterCreate(buf, KeyRingCreate(kp), KeyRingCreate())
+		defer decrypter.Close()
+		reader := decrypter.Decrypt()
+		data, err := ioutil.ReadAll(reader)
+		util.Check(err)
+		assert.Equal(t, "first", string(data))
+	}()
+
+	// d, err := ioutil.ReadAll(buf)
+	// util.Check(err)
+	// log.Printf("rest: %s", string(d))
+
+	func() {
+		decrypter := SymDecrypterCreate(buf, s)
+		defer decrypter.Close()
+		reader := decrypter.Decrypt()
+		data, err := ioutil.ReadAll(reader)
+		util.Check(err)
+		assert.Equal(t, "second", string(data))
+	}()
+
+}
