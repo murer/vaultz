a97e5da9216fbb5a0c1405ea7b0a8521768e9ea4
murer <murer@dextra-sw.com>
2021-04-13 22:30:28 -0300

    init

--- pgp/symencrypter.go
+++ pgp/symencrypter.go
@@ -17,6 +17,7 @@ type SymEncrypter struct {
 	ciphered io.Writer
 	key      *SymKey
 
+	armor  io.WriteCloser
 	writer io.WriteCloser
 
 	byteCount uint64
@@ -37,6 +38,7 @@ func (me *SymEncrypter) Encrypt() io.WriteCloser {
 	util.Check(err)
 	ew, err := openpgp.SymmetricallyEncrypt(wa, me.key.key, nil, packetConfig)
 	util.Check(err)
+	me.armor = wa
 	me.writer = ew
 	log.Printf("SymEncrypt start, key size: %d", me.key.Size())
 	return me
@@ -49,7 +51,12 @@ func (me *SymEncrypter) Write(p []byte) (n int, err error) {
 
 func (me *SymEncrypter) Close() error {
 	log.Printf("SymEncrypt done, size: %d", me.byteCount)
-	return me.writer.Close()
+	we := me.writer.Close()
+	ae := me.armor.Close()
+	if we != nil {
+		return we
+	}
+	return ae
 }
 
 func _symEncryptBytes(plain []byte, key *SymKey) *bytes.Buffer {
