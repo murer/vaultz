2f8f07bbe8a6c78d1ccb7045df8f77fbffe3e383
murer <murer@dextra-sw.com>
2021-04-15 16:01:26 -0300

    x

--- pgp/cryptencypt_test.go
+++ pgp/cryptencypt_test.go
@@ -2,42 +2,10 @@ package pgp
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func TestKeyGen(t *testing.T) {
-	assert.Equal(t, 1, 1)
-
-	kp := KeyGenerate("test", "test@sample.com")
-	assert.Equal(t, "test", kp.UserName())
-	assert.Equal(t, "test@sample.com", kp.UserEmail())
-	assert.NotEmpty(t, kp.ExportPub())
-	assert.Regexp(t, "-----END PGP PUBLIC KEY BLOCK-----$", kp.ExportPub())
-	assert.NotEmpty(t, kp.ExportPriv())
-	assert.Regexp(t, "-----END PGP PRIVATE KEY BLOCK-----$", kp.ExportPriv())
-	assert.NotEmpty(t, kp.Id())
-
-	fmt.Printf("id: %s\n", kp.Id())
-
-	pubkp := KeyImport(kp.ExportPub())
-	assert.Equal(t, "test", pubkp.UserName())
-	assert.Equal(t, "test@sample.com", pubkp.UserEmail())
-	assert.Equal(t, kp.ExportPub(), pubkp.ExportPub())
-	assert.Empty(t, pubkp.ExportPriv())
-	assert.Equal(t, kp.Id(), pubkp.Id())
-	fmt.Printf("id: %s\n", kp.Id())
-
-	privkp := KeyImport(kp.ExportPriv())
-	assert.Equal(t, "test", privkp.UserName())
-	assert.Equal(t, "test@sample.com", privkp.UserEmail())
-	assert.Equal(t, kp.ExportPub(), privkp.ExportPub())
-	assert.NotEmpty(t, privkp.ExportPriv())
-	assert.Equal(t, kp.Id(), privkp.Id())
-	fmt.Printf("id: %s\n", privkp.Id())
-}
 
 func TestCryptWrongDecrypt(t *testing.T) {
 	maria := KeyGenerate("maria", "maria@sample.com")
