add8e35c0cb8fc6133080cee5a20e292f5c38477
murer <murer@ciandt.com>
2024-04-07 09:56:32 -0300

    init

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@
 _vaultz_bin="${VAULTZ_BIN:-go run main.go}"
 
 function cmd_prepare() {
-    rm -rf gen/vaultz || true
-    mkdir -p gen/vaultz
+    rm -rf test/vaultz || true
+    mkdir -p test/vaultz
 }
 
 function cmd_test_help() {
