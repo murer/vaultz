158eeeeabdcf6828566df9995dc2e32f3555b1c3
murer <murer@ciandt.com>
2024-04-07 09:22:33 -0300

    init

--- test.sh
+++ test.sh
@@ -1,11 +1,18 @@
 #!/bin/bash -xe
 
+_vaultz_bin="${VAULTZ_BIN:-go run main.go}"
+
+function cmd_help() {
+    $_vaultz_bin "--help"
+}
+
 function cmd_basics() {
     true
 }
 
 function cmd_all() {
     cmd_basics
+    cmd_help
 }
 
 cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"
