3e7d4ac128bf9128a2acd88e17a17c7bdb9b0ac7
murer <murer@ciandt.com>
2024-04-07 19:12:54 -0300

    init

--- poc/poc3/main.go
+++ poc/poc3/main.go
@@ -10,9 +10,11 @@ import (
 func main() {
 	buf := new(bytes.Buffer)
 	x, err := armor.Encode(buf, "AAAA", nil)
+	// x := base64.NewEncoder(base64.StdEncoding, buf)
 	if err != nil {
 		panic(err)
 	}
 	x.Write([]byte("bla"))
+	x.Close()
 	fmt.Println(buf.String())
 }
