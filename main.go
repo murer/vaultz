faf1b1b979c843bee1ef32084751f609661f2e4c
murer <murer@ciandt.com>
2024-04-07 11:40:44 -0300

    init

--- main.go
+++ main.go
@@ -1,24 +1,10 @@
 package main
 
 import (
-	"bytes"
-	"crypto"
-	"log"
-
-	"golang.org/x/crypto/openpgp"
-	"golang.org/x/crypto/openpgp/armor"
-	"golang.org/x/crypto/openpgp/packet"
+	"flag"
+	"fmt"
+	"os"
 )
-
-var Config = &packet.Config{
-	DefaultHash:            crypto.SHA256,
-	DefaultCipher:          packet.CipherAES256,
-	DefaultCompressionAlgo: packet.CompressionZLIB,
-	CompressionConfig: &packet.CompressionConfig{
-		Level: packet.BestCompression,
-	},
-	RSABits: 512,
-}
 
 func Check(err error) {
 	if err != nil {
@@ -26,69 +12,43 @@ func Check(err error) {
 	}
 }
 
-func ArmorIn(key *packet.PublicKey) string {
-	buf := new(bytes.Buffer)
-	func() {
-		writer, ret := armor.Encode(buf, openpgp.PublicKeyType, nil)
-		Check(ret)
-		defer writer.Close()
-		key.Serialize(writer)
-	}()
-	return buf.String()
+func parseCommands(args []string) {
+	subcommand := "help"
+	if len(args) <= 1 {
+		subcommand = args[0]
+		args = args[1:]
+	}
+	fmt.Printf(subcommand)
 }
 
 func main() {
-	fromKP, err := openpgp.NewEntity("John1", "Testing", "johndoe@example.com", Config)
-	Check(err)
-	log.Printf("From key pair: %v", fromKP.PrimaryKey.KeyIdString())
-	log.Println(ArmorIn(fromKP.PrimaryKey))
+	parseCommands(os.Args)
+	if len(os.Args) < 2 {
+		fmt.Println("Expected a subcommand")
+		os.Exit(1)
+	}
 
-	dstKP1, err := openpgp.NewEntity("John2", "Testing", "johndoe@example.com", Config)
-	Check(err)
-	log.Printf("Dest key pair: %v", dstKP1.PrimaryKey.KeyIdString())
-	log.Println(ArmorIn(dstKP1.PrimaryKey))
+	// Subcommands
+	serveCmd := flag.NewFlagSet("serve", flag.ExitOnError)
+	servePort := serveCmd.Int("port", 8080, "Port to run the server on")
 
-	dstKP2, err := openpgp.NewEntity("John3", "Testing", "johndoe@example.com", Config)
-	Check(err)
-	log.Printf("Dest key pair: %v", dstKP2.PrimaryKey.KeyIdString())
-	log.Println(ArmorIn(dstKP2.PrimaryKey))
+	migrateCmd := flag.NewFlagSet("migrate", flag.ExitOnError)
+	migrateDir := migrateCmd.String("dir", "./migrations", "Directory with migration files")
 
-	var dests openpgp.EntityList
-	dests = append(dests, dstKP1)
-	dests = append(dests, dstKP2)
+	// Check which subcommand is invoked
+	switch os.Args[1] {
+	case "serve":
+		// Parse flags for the 'serve' subcommand
+		serveCmd.Parse(os.Args[2:])
+		fmt.Printf("Serving on port %d...\n", *servePort)
 
-	buf := new(bytes.Buffer)
-	log.Printf("Encrypting")
-	func() {
-		cwriter, err := openpgp.Encrypt(buf, dests, fromKP, nil, Config)
-		Check(err)
-		defer cwriter.Close()
-		cwriter.Write([]byte("mymsg"))
-	}()
+	case "migrate":
+		// Parse flags for the 'migrate' subcommand
+		migrateCmd.Parse(os.Args[2:])
+		fmt.Printf("Running migrations from directory '%s'...\n", *migrateDir)
 
-	log.Printf("Encrypted: %x", buf.Bytes())
-
-	var keys openpgp.EntityList
-	keys = append(keys, dstKP1)
-	keys = append(keys, dstKP2)
-	keys = append(keys, fromKP)
-
-	log.Printf("Decrypting")
-	reader := bytes.NewReader(buf.Bytes())
-	msg, err := openpgp.ReadMessage(reader, keys, nil, Config)
-	Check(err)
-	log.Printf("isSigned: %v", msg.IsSigned)
-	log.Printf("SignatureError: %#v", msg.SignatureError)
-	log.Printf("SignedByKeyId: %x", msg.SignedByKeyId)
-	log.Printf("SignedBy.KeyIdString: %s", msg.SignedBy.PublicKey.KeyIdString())
-	log.Printf("SignedBy.Fingerprint: %x", msg.SignedBy.PublicKey.Fingerprint)
-	log.Printf("SignedBy.PublicKey: %s", ArmorIn(msg.SignedBy.PublicKey))
-	// log.Printf("SignedByKeyId: %v", msg.SignedBy.PrivateKey)
-	log.Printf("IsSymmetricallyEncrypted: %b", msg.IsSymmetricallyEncrypted)
-	log.Printf("Signature: %#v", msg.Signature)
-	log.Printf("isEncrypted: %v", msg.IsEncrypted)
-	buf = &bytes.Buffer{}
-	buf.ReadFrom(msg.LiteralData.Body)
-	data := buf.String()
-	log.Printf("Decrypted: %s", data)
+	default:
+		fmt.Println("Expected 'serve' or 'migrate' subcommands")
+		os.Exit(1)
+	}
 }
